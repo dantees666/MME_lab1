0e96edfbc3cb74da340acdac1220042dd029c99e
Andrew Buyanov <dantees1a@gmail.com>
2023-04-06 15:52:11 +0700

    Initial Commit

---
+++ MME_Lab_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MME_Lab_1
+//
+//  Created by Андрей Буянов on 06.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
